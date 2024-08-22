d419b5fed37dc68b7a9244e24a3d7a86c97ddb3e
Denalda ☀️ <denaldabali@gmail.com>
2024-08-23 01:10:47 +0200

    React App with Welcome Message
    Created a new React app with an initial message: Hello world, this is my first react app.

---
+++ React Intro/react-intro/src/App.js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+        Hello world! 🌍<br></br>This is my first React App 🎈✨<br></br>
+        Oops, it's still in Beta Plan and figuring things out! 🛠️<br></br>
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
